322511220b26d8e1fe6368f6a6d6e02afb47c1f7
mexriddin19 <gsalohiddin29@gmail.com>
2024-03-13 21:35:58 +0500

    8chi dars bu

---
+++ main.js
@@ -0,0 +1,29 @@
+// let str = ["abduloh", "javlonbek", "mexriddin", "abduloh ravshanov"];
+// let con = confirm("ogohlantirish");
+// if (con) {
+//   console.log(str.splice(0, 2));
+// } else {
+//   console.log(str.splice(2));
+// }
+let numbers = [2, 4, 9, 10, 45, 67, 8, 90];
+
+let juftSonlar = [];
+let toqSonlar = [];
+
+console.log(juftSonlar);
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) {
+    juftSonlar.push(numbers[i]);
+  } else {
+    toqSonlar.push(numbers[i]);
+  }
+}
+
+console.log(juftSonlar);
+console.log(toqSonlar);
+
+
+for(let i = 0; i < 100; i++) {
+  //...............
+}
